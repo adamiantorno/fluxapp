45bb62c21b1de05efe8b61f8701a277732aee9de
Adam I <adam.iantorno99@gmail.com>
2021-06-30 00:26:28 -0400

    add scss compiling

--- frons/webpack.config.js
+++ frons/webpack.config.js
@@ -11,8 +11,14 @@ module.exports = {
         use: ["babel-loader"],
       },
       { 
-        test: /\.css$/, 
-        use: ['style-loader', 'css-loader'] 
+        test: /\.scss$/, 
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader" 
+        }, {
+          loader: "sass-loader"
+        }]
       }
     ],
   },
